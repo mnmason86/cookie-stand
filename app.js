a4c73f8b2dd12a9c4d6dea2881f28f560fc41f72
Mandy Mason <mnmason86@gmail.com>
2022-05-18 23:46:09 -0400

    added math.random function for hourly calculation to app.js. added ul id's to sales page.

---
+++ app.js
@@ -0,0 +1,9 @@
+// Min max customer calculation for location simulation
+
+function randBetween(min,max){
+  return min + Math.random() * (max-min);
+}
+console.log(randBetween(23,65));
+
+// Seattle stats: min customers: 23, max customers: 65, avg cookies/customer: 6.3
+
