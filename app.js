c57db708e70faf41b0f2d7db4e7af1bb95789747
Mandy Mason <mnmason86@gmail.com>
2022-05-21 20:22:57 -0400

    replaced object literals with constructor function. appended daily hours to table and applied css to table.

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
+'use strict';
 
 // Store locations with min customers/hour, max customers/hour, and avg cookies per customer
 
-let locations = [
+// Constructor function
 
-  {
-    site: 'Seattle',
-    minCust: 23,
-    maxCust: 65,
-    avgCookies: 6.3,
-  },
-  {
-    site: 'Tokyo',
-    minCust: 3,
-    maxCust: 24,
-    avgCookies: 1.2,
-  },
-  {
-    site: 'Dubai',
-    minCust: 11,
-    maxCust: 38,
-    avgCookies: 3.7,
-  },
-  {
-    site: 'Paris',
-    minCust: 20,
-    maxCust: 38,
-    avgCookies: 2.3,
-  },
-  {
-    site: 'Lima',
-    minCust: 2,
-    maxCust: 16,
-    avgCookies: 4.6,
-  },
-];
+function Location(site,minCust,maxCust,avgCookies){
+  this.site = site;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookies = avgCookies;
+  this.randomCookies = Math.round(minCust + Math.random() * (maxCust - minCust));
+}
+
+let seattleLocation = new Location('Seattle', 23, 65, 6.3);
+let tokyoLocation = new Location('Tokyo', 3, 24, 1.2);
+let dubaiLocation = new Location('Dubai', 11, 38, 3.7);
+let parisLocation = new Location('Paris', 20, 38, 2.3);
+let limaLocation = new Location('Lima', 2, 16, 4.6);
+
+let locations = [seattleLocation, tokyoLocation, dubaiLocation, parisLocation, limaLocation];
 
 //Daily shop hours
 
@@ -41,36 +26,53 @@ let dailyHours = ['6:00 am','7:00 am','8:00 am','9:00 am','10:00 am','11:00 am',
 
 // Total Daily Sales function
 
-function sumArray (array){
-  let sum = 0;
-  array.forEach(item => {
-    sum += item;
-  });
-  return sum;
-}
+// function sumArray (array){
+//   let sum = 0;
+//   array.forEach(item => {
+//     sum += item;
+//   });
+//   return sum;
+// }
+
+//
+
+Location.prototype.renderHours = function(){
+  let rowContainer = document.getElementById('hours-row');
+
+  let rowData = document.createElement('tr');
+
+  for (let i = 0; i < dailyHours.length; i++){
+    let dataCell = document.createElement('td');
+    dataCell.innerText = dailyHours[i];
+    rowData.appendChild(dataCell);
+  }
+  rowContainer.appendChild(rowData);
+};
+
+Location.prototype.renderHours();
 
 //Loop to generate sales table
 
-for (let i = 0; i < locations.length; i++){
-  let location = locations[i];
-  let cookieSales = [];
+// for (let i = 0; i < locations.length; i++){
+//   let location = locations[i];
+//   let cookieSales = [];
 
-  for(let j = 0; j < dailyHours.length; j++){
-    let hour = dailyHours[j];
-    let custPerHour = Math.round(location.minCust + Math.random() * (location.maxCust - location.minCust));
-    let hourCookies = Math.round(custPerHour * location.avgCookies);
-    cookieSales.push(hourCookies);
+//   for(let j = 0; j < dailyHours.length; j++){
+//     let hour = dailyHours[j];
+//     let custPerHour = Math.round(location.minCust + Math.random() * (location.maxCust - location.minCust));
+//     let hourCookies = Math.round(custPerHour * location.avgCookies);
+//     cookieSales.push(hourCookies);
 
-    let cookieSummary = document.createElement('li');
-    cookieSummary.innerText = `${hour}: ${hourCookies} cookies sold in ${location.site}`;
-    document.getElementById('salmonCookies').appendChild(cookieSummary);
-  }
+//     let cookieSummary = document.createElement('li');
+//     cookieSummary.innerText = `${hour}: ${hourCookies} cookies sold in ${location.site}`;
+//     document.getElementById('salmonCookies').appendChild(cookieSummary);
+//   }
 
-  sumArray(cookieSales);
-  console.log(sumArray(cookieSales));
-  console.log (cookieSales);
+//   sumArray(cookieSales);
+//   console.log(sumArray(cookieSales));
+//   console.log (cookieSales);
 
-  let dailyTotal = document.createElement('li');
-  dailyTotal.innerText = `${sumArray(cookieSales)} cookies sold today in ${location.site}`;
-  document.getElementById('salmonCookies').appendChild(dailyTotal);
-}
+//   let dailyTotal = document.createElement('li');
+//   dailyTotal.innerText = `${sumArray(cookieSales)} cookies sold today in ${location.site}`;
+//   document.getElementById('salmonCookies').appendChild(dailyTotal);
+// }
